94ab2ca860a326c008ec416f087dcaef0aad48f0
yonroa <yonanderson11@gmail.com>
2022-07-26 16:36:52 -0500

    Add advanced task 19 and 20 and fic Betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,19 +6,20 @@
  * @second: pointer to the second node
  * Return: a pointer to the lowest common ancestor node of the two given nodes
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
-    binary_tree_t *mom, *father;
+	binary_tree_t *mom, *father;
 
-    if (!first || !second)
-        return (NULL);
-    if (first == second)
-        return ((binary_tree_t *)first);
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    mom = first->parent, father = second->parent;
-    if (first == father || !mom || (!mom->parent && father))
-        return (binary_trees_ancestor(first, father));
-    else if (mom == second || !father || (!father->parent && mom))
-        return (binary_trees_ancestor(mom, second));
-    return (binary_trees_ancestor(mom, father));
+	mom = first->parent, father = second->parent;
+	if (first == father || !mom || (!mom->parent && father))
+		return (binary_trees_ancestor(first, father));
+	else if (mom == second || !father || (!father->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, father));
 }
