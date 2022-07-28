9124ebe83715d7a6d9e3c8d56662891f3e01da98
yonroa <yonanderson11@gmail.com>
2022-07-28 13:41:51 -0500

    changes in function height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,12 +37,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left = 0, right = 0;
 
 	if (tree == NULL)
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left += binary_tree_height(tree->left);
+	right += binary_tree_height(tree->right);
 
 	if (left > right)
 		return (left + 1);
