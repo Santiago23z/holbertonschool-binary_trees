88176d5739e9f3be1a873bf0f9f980a7562a8388
yonroa <yonanderson11@gmail.com>
2022-07-28 13:55:51 -0500

    Back to the original

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,11 +7,13 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int right = 0, left = 0;
+
 	if (tree == NULL)
 		return (0);
-	if ((binary_tree_is_full(tree) == 1) && (binary_tree_balance(tree) == 0))
-		return (1);
-	return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return ((left == right) && binary_tree_is_full(tree));
 }
 
 /**
@@ -25,11 +27,10 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-	return (0);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
-
 
 /**
  * binary_tree_height - measures the height of a binary tree
@@ -38,33 +39,15 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left = 0;
-	size_t right = 0;
+	size_t left, right;
 
 	if (tree == NULL)
 		return (0);
-
-
-	left += 1 + binary_tree_height(tree->left);
-	right += 1 + binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
 	if (left > right)
-		return (left);
-	return (right);
-}
-
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to count the number of leaves
- * Return: leaves in a binary tree
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	size_t left_height = 0, right_height = 0;
-
-	if (tree == NULL)
-		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	return (left_height - right_height);
+		return (left + 1);
+	else
+		return (right + 1);
 }
