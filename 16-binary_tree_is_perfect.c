1bec042ed5b1636a46969d84c442592cee0470a9
yonroa <yonanderson11@gmail.com>
2022-07-28 13:20:17 -0500

    add conditional NULL in perfect function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int right = 0, left = 0;
 
+	if (tree == NULL)
+		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if ((left == right) && binary_tree_is_full(tree) == 1)
